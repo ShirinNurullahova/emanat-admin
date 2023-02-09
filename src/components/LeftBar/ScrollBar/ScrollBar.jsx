ee228e457c7507da66af7228c8f059027db602de
Khanlarr <xanlar.beybalayev@gmail.com>
2023-02-09 17:40:19 +0400

    terminal_map

--- src/components/LeftBar/ScrollBar/ScrollBar.jsx
+++ src/components/LeftBar/ScrollBar/ScrollBar.jsx
@@ -8,6 +8,7 @@ import HashTag from "./HashTag/HashTag";
 const ScrollBar = () => {
   const [toggle, setToggle] = useState(true);
   const [toggle2, setToggle2] = useState(true);
+  const [toggle3, setToggle3] = useState(true);
   return (
     <>
       <ProfileMain />
@@ -145,6 +146,55 @@ const ScrollBar = () => {
                 <Link to="/terminal">
                   <div className="scroll-bar-dropdown-el-p">
                     <p>Terminal</p>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          )}
+
+          <div
+            className="scroll-bar-el-down"
+            onClick={() => setToggle3(!toggle3)}
+          >
+            <div className="scroll-bar-el-down-div">
+              <svg className="svg-icon" viewBox="0 0 640 512">
+                <path d="M41.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 41.4 86.6zM288 416H576c17.7 0 32 14.3 32 32s-14.3 32-32 32H288c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/>
+                </svg>
+              <div className="scroll-bar-el-down-div-text">
+                <p>Terminals</p>
+              </div>
+            </div>
+            <div className="scroll-bar-el-down-svg">
+              <svg className="svg-icon-two" viewBox="0 0 20 20">
+                <path
+                  fill="none"
+                  d="M13.388,
+                             9.624h-3.011v-3.01c0-0.208-0.168-0.377-0.376-0.377S9.624,
+                             6.405,
+                             9.624,
+                             6.613v3.01H6.613c-0.208,
+                             0-0.376,0.168-0.376,
+                             0.376s0.168,
+                             0.376,
+                             0.376,
+                             0.376h3.011v3.01c0,
+                             0.208,
+                             0.168
+                             ,0.378
+                             ,0.376,
+                             0.378s0.376-0.17,
+                             0.376-0.378v-3.01h3.011c0.207,
+                          9.624,13.388,9.624z M10,1.344c-4.781,0-8.656,3.875-8.656,8.656c0,4.781,3.875,8.656,8.656,8.656c4.781,0,8.656-3.875,8.656-8.656C18.656,5.219,14.781,1.344,10,1.344z M10,17.903c-4.365,0-7.904-3.538-7.904-7.903S5.635,2.096,10,2.096S17.903,5.635,17.903,10S14.365,17.903,10,17.903z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+          {!toggle3 && (
+            <div className="scroll-bar-dropdown">
+              <div className="scroll-bar-dropdown-el">
+                <Link to="/map">
+                  <div className="scroll-bar-dropdown-el-p">
+                    <p>Terminal Map</p>
                   </div>
                 </Link>
               </div>
