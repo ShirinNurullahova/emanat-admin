5d22eab6b7eb340799154803f13a43de83f437e4
Khanlarr <xanlar.beybalayev@gmail.com>
2023-02-14 15:29:22 +0400

    dddf

--- src/components/LeftBar/Vakansiyalar/VakansiyaJob/VakansiyaCard/VakansiyaCard.jsx
+++ src/components/LeftBar/Vakansiyalar/VakansiyaJob/VakansiyaCard/VakansiyaCard.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import ElementCard from './ElementCard/ElementCard';
+import VakansiyaPostItem from './VakansiyaPostItem/VakansiyaPostItem';
+import VakansiyaPost from './VakansiyaPost/VakansiyaPost';
 
 const VakansiyaCard = ({ btn, setBtn }) => {
 
     const [data, setData] = useState(null);
     const [button, setButton] = useState(false);
+    const [button1, setButton1] = useState(false);
     const [id, setId] = useState('');
+    const [idPost, setIdPost] = useState('');
 
+    const [btnAdd,setBtnAdd]=useState(false);
 
     const fetchData = () => {
         axios.get((`${process.env.REACT_APP_URL}/admin/vacation/job`), {
@@ -42,6 +47,7 @@ const VakansiyaCard = ({ btn, setBtn }) => {
                             <th>{data && data[0]?.sections[1]?.title}</th>
                             <th></th>
                             <th></th>
+                            <th onClick={()=>setBtnAdd(true)}>Add</th>
                         </tr>
                     </thead>
                     {
@@ -51,6 +57,10 @@ const VakansiyaCard = ({ btn, setBtn }) => {
                                 <tr>
                                     <td>{e && e?.sections[0]?.items}</td>
                                     <td>{e && e?.sections[1]?.items}</td>
+                                    <td onClick={() => {
+                                        setButton1(true)
+                                        setIdPost(e?.id)
+                                    }}>Add</td>
                                     <td onClick={() => {
                                         setButton(true)
                                         setId(e?.id)
@@ -66,7 +76,13 @@ const VakansiyaCard = ({ btn, setBtn }) => {
                
             </div>
             {
+               btnAdd && <VakansiyaPost btnAdd={btnAdd} setBtnAdd={setBtnAdd}/>
+            }
+            {
                     button && <ElementCard id={id} setButton={setButton} button={button} />
+                }
+                {
+                    // button1 && <VakansiyaPostItem id={idPost} setButton={setButton1} button={button1}/>
                 }
         </div>
 
